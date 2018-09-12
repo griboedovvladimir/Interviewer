73a1dab70913a19320f218802cf943654eb69fa8
Vladimir <likecoffee@yandex.ru>
2018-09-12 10:20:40 +0300

    visualized block-switcher by jQuery

--- index.js
+++ index.js
@@ -11,15 +11,26 @@ $('body').bind('focusin', function (e) {
 });
 
 $('.block-switcher').click(function (e) {
+    function unActiveBlock(){
+        for(let el of $('.block-switcher').children()){
+            if(el.classList.contains('question-block-active')){
+                el.classList.remove('question-block-active')
+            }
+        }
+    }
+
     if (e.target.id === 'CSSSwitcherBlock' || e.target.parentNode.id === 'CSSSwitcherBlock') {
+        unActiveBlock()
         $('.question-block > div').text('CSS');
         $('#CSSSwitcherBlock').addClass('question-block-active');
     }
     else if (e.target.id === 'HTMLSwitcherBlock' || e.target.parentNode.id === 'HTMLSwitcherBlock') {
+        unActiveBlock()
         $('.question-block > div').text('HTML');
         $('#HTMLSwitcherBlock').addClass('question-block-active');
     }
     else if (e.target.id === 'JSSwitcherBlock' || e.target.parentNode.id === 'JSSwitcherBlock') {
+        unActiveBlock()
         $('.question-block > div').text('JS');
         $('#JSSwitcherBlock').addClass('question-block-active');
     }
