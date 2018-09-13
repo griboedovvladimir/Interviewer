539edd79434887c6b63e0827ddb73bea91828fbc
Vladimir <likecoffee@yandex.ru>
2018-09-13 17:54:29 +0300

    Aded webpack bundler

---
+++ webpack.config.js
@@ -0,0 +1,124 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path= require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const autoprefixer = require('autoprefixer');
+
+let conf = {
+    entry:{
+        main: path.resolve(__dirname,'./src/app/index.js')
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname,'./dist'),
+    },
+    resolve: {
+        extensions: ['.js']
+    },
+    module:{
+        rules:[
+            {
+                test: /\.less$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: [
+                                autoprefixer({
+                                    browsers: ['ie >= 8', 'last 2 version']
+                                })
+                            ],
+                        }
+                    },
+                    {
+                        loader: 'less-loader',
+                    }
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: [
+                                autoprefixer({
+                                    browsers: ['ie >= 8', 'last 2 version']
+                                })
+                            ],
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                    }
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]',
+                    outputPath: 'fonts/'
+                }
+            },
+            {
+                test: /\.(gif|jpg?g|png|svg)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]',
+                    outputPath: 'img/'
+                }
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "style.css",
+        }),
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'src/index.html',
+            inject: false,
+            chunks:{
+                head:'style.css',
+                js: 'main.js'
+            }
+        })
+    ],
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true
+                //,sourceMap: true
+            }),
+            new OptimizeCSSAssetsPlugin({})
+        ]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        compress: true,
+        port: 3030,
+        overlay:true
+    },
+    ///watch:true
+};
+
+module.exports = (env,options)=>{
+    let production = options.mode === 'production';
+    conf.devtool = production
+        ? false
+        : 'source-map';
+   return conf;
+};
