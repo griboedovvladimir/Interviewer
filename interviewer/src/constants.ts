229c9baebf9cc4475c47766e640724617dbb900b
Vladimir <likecoffee@yandex.ru>
2018-09-26 17:53:45 +0300

    Redux err

--- interviewer/src/constants.ts
+++ interviewer/src/constants.ts
@@ -1,6 +1,7 @@
+
 /*---------------- API CALLS ----------------------*/
 
-export const LOGED_API_RES = 'true';
+export const LOGGED_API_RES = 'true';
 
 /*----------------- ROUTES -----------------------*/
 
@@ -8,6 +9,9 @@ export const LOGIN_PAGE = '/login/';
 export const MAIN_PAGE = '/main/';
 
 /*-------------- ACTIONS CONSTANTS ---------------*/
+
+export const ACTION_LOGIN = 'LOGIN';
+export const ACTION_LOGOUT = 'LOGOUT';
 
 export const ACTION_ADD_INTERVIEW = 'ADD_INTERVIEW';
 export const ACTION_REMOVE_INTERVIEW = 'REMOVE_INTERVIEW';
--- interviewer/src/reducers/reducers.ts
+++ interviewer/src/reducers/reducers.ts
@@ -2,23 +2,31 @@ import * as CONSTANTS from '../constants';
 import {guid} from "../helpers/guid";
 import {getDateByString} from "../helpers/getDateByString";
 import {InterviewInterface} from "../interfaces/interview.interface";
-const defultProjects:InterviewInterface[] = [];
 
-export function interviewReducer(state = defultProjects, action: any) {
-    if (action.type === CONSTANTS .ACTION_ADD_INTERVIEW) {
-        let newState = [
+const defultProjects: InterviewInterface[] = [];
+
+export function login(state = {logged: 'false'}, action: any) {
+    if (action.type === CONSTANTS.ACTION_LOGIN) {
+        return {
+            logged: action.logged,
+        };
+    }
+    return state;
+}
+
+export function interview(state = defultProjects, action: any) {
+    if (action.type === CONSTANTS.ACTION_ADD_INTERVIEW) {
+        return [
             {
                 id: guid(),
                 name: action.name,
-                level:action.level,
+                level: action.level,
                 specialization: action.specialization,
                 date: getDateByString(),
                 status: 'ok'
             },
             ...state
         ];
-        localStorage.setItem('Projects',JSON.stringify(newState));
-        return newState;
     }
     return state;
 }
