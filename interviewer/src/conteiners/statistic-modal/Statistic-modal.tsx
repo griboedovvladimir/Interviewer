7fc9f3605d417b2b503bf742126cf88071bf586e
Vladimir <likecoffee@yandex.ru>
2018-10-22 18:33:03 +0300

    Added modal to charts

---
+++ interviewer/src/conteiners/statistic-modal/Statistic-modal.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import {Component} from 'react';
+
+export class StatisticModal extends Component {
+    constructor(props:any){
+        super(props)
+    }
+
+    public render(){
+        return (
+            <h1></h1>
+        )
+
+}
