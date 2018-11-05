6f7870f3e3c09051acdc1a54bbf76410f0884f9d
Vladimir <likecoffee@yandex.ru>
2018-11-05 18:28:11 +0300

    Added menu admin page

---
+++ interviewer/src/conteiners/administration-menu/Administration-menu.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+import {Component} from 'react';
+
+export class AdministrationMenu extends Component{
+    public props:any;
+    public state:any;
+    constructor(props:any){
+        super(props)
+    }
+
+    public render(){
+        return(
+            <h1></h1>
+        )
+    }
+
+}
